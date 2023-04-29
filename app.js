fd0c302f580cb94adfe4aa4bc4c4faa6af527ca7
Voltrein-code <mr-votlrein@yandex.ru>
2023-04-29 15:56:27 +0300

    feature: Добавить переформатирование кодов маркировки

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
+const { getNewExcel } = require('./controllers/excelForCrpt');
 
 const PORT = 3000;
 
 const app = express();
+
+getNewExcel();
 
 app.listen(PORT);
--- controllers/excelForCrpt.js
+++ controllers/excelForCrpt.js
@@ -1,6 +1,19 @@
-const fs = require('fs/promises');
-const xlsx = require("xlsx");
+// const fs = require('fs/promises');
+const xlsx = require('xlsx');
 
-module.exports.returnValidExcel(req, res, next) = {
+module.exports.getNewExcel = () => {
+  const file = 'input.xls';
 
-}
+  const stringsArray = xlsx.readFile(file).Strings;
+
+  const test = [...new Set(stringsArray.map((item) => {
+    const cell = item.t;
+    return [cell.toString().slice(0, 24)];
+  }))];
+  const book = xlsx.utils.book_new();
+  const sheet = xlsx.utils.aoa_to_sheet(test);
+
+  xlsx.utils.book_append_sheet(book, sheet, '1');
+
+  xlsx.writeFile(book, 'result.xls');
+};
